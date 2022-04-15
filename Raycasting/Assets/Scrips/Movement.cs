5cab45b5cee5306a5931142caa1e5682cb331f16
martin <martinconcetti97@gmail.com>
2022-04-15 12:54:13 -0300

    Invicible limits and new player

---
+++ Raycasting/Assets/Scrips/Movement.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private int movementSpeed;
+    private Rigidbody body;
+    private bool left;
+    private bool right;
+    private bool up;
+    private bool down;
+
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            left = true;
+        }
+        else
+        {
+            left = false;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            right = true;
+        }
+        else
+        {
+            right = false;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            up = true;
+        }
+        else
+        {
+            up = false;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            down = true;
+        }
+        else
+        {
+            down = false;
+        }
+
+     
+    }
+
+    private void FixedUpdate()
+    {
+        if (left)
+        {
+            body.AddForce(new Vector3(-movementSpeed, 0, 0));
+
+        }
+
+        if (right)
+        {
+            body.AddForce(new Vector3(movementSpeed, 0, 0));
+
+        }
+
+        if (up)
+        {
+            body.AddForce(new Vector3(0, 0, movementSpeed));
+
+        }
+
+        if (down)
+        {
+            body.AddForce(new Vector3(0, 0, -movementSpeed));
+
+        }
+
+        
+    }
+}
